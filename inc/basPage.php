db08e2e7d2dbfafde014bca70ef867506fc635d6
Matteo <Matteo@Altreon.home>
2016-02-16 22:08:20 +0100

    .

--- inc/basPage.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '
-		<div class="contact">
-		<button class="contact"><span>Contact</span></button>
-		
-		</div>
-';
--- inc/titre.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '
-	<div class="titre">
-		<img class="titre" src="img/geekzone.png"></img>
-	</div>
-';
