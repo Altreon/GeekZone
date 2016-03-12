fbcad421da711b03e0f06060135ae0bd6cc08141
Altreon <etudiant@w7-b317-02-mb>
2016-03-12 03:43:26 +0100

    .

--- inc/titre.php
+++ inc/titre.php
@@ -2,5 +2,11 @@
 echo '
 	<div class="titre">
 		<a href="index.php"><img class="titre" src="img/geekzone.png"></img></a>
+		';
+		if(!isset($page)){
+			echo'
+			<a href="boutiquesmap.php"><button class="boutiques"><span>Boutiques</span></button></a>
+			<a href="categorieProduit.php"><button class="produits"><span>Produit</span></button></a>
+		';}echo'
 	</div>
 ';
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ echo '
 <link rel="stylesheet" type="text/css" media="screen" href="css/GeekZone.css"/>
 	';
 	
+$page = "Acceuil";
 include 'inc/titre.php';	
 	
 echo '
