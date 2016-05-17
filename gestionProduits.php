36f88d7bbb033d0adb41a7ec8a92151d7e24b203
etudiant <etudiant@etudiant-PC>
2016-05-17 16:08:19 +0200

    .

--- gestionProduits.php
+++ gestionProduits.php
@@ -31,46 +31,52 @@ echo '
 	<div class = "content">
 ';
 
-if(isset($_POST['envoyerAjout']) && !empty($_POST['envoyerAjout']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['description']) && !empty($_POST['description']) &&isset($_POST['detail']) && !empty($_POST['detail']) &&isset($_POST['prix']) && !empty($_POST['prix']) && isset($_POST['image']) && !empty($_POST['image']) &&isset($_POST['categorie']) && !empty($_POST['categorie'])){
-	insertTableauProduit($base, $hote, $utilisateur, $mdp, $_POST['nom'], $_POST['description'], $_POST['detail'], $_POST['prix'], $_POST['image'], $_POST['categorie']);
+if (!isset($_SESSION['logCompte']) || $_SESSION['statCompte'] != "G") {  // Vérification des droits pour gérer les utilisateurs
+	echo'<p class="blocAcces">Vous ne possédez pas les autorisations nécessaires pour visionner le contenu de cette page!</p>';
+} else {
+
+	if(isset($_POST['envoyerAjout']) && !empty($_POST['envoyerAjout']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['description']) && !empty($_POST['description']) &&isset($_POST['detail']) && !empty($_POST['detail']) &&isset($_POST['prix']) && !empty($_POST['prix']) && isset($_POST['image']) && !empty($_POST['image']) &&isset($_POST['categorie']) && !empty($_POST['categorie'])){
+		insertTableauProduit($base, $hote, $utilisateur, $mdp, $_POST['nom'], $_POST['description'], $_POST['detail'], $_POST['prix'], $_POST['image'], $_POST['categorie']);
+	}
+	
+	if(isset($_POST['envoyerModif']) && !empty($_POST['envoyerModif']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['description']) && !empty($_POST['description']) &&isset($_POST['detail']) && !empty($_POST['detail']) &&isset($_POST['prix']) && !empty($_POST['prix']) && isset($_POST['image']) && !empty($_POST['image']) &&isset($_POST['categorie']) && !empty($_POST['categorie']) && isset($_POST['hdIdProduit']) && !empty($_POST['hdIdProduit'])){
+		updateTableauProduit($base, $hote, $utilisateur, $mdp, $_POST['nom'], $_POST['description'], $_POST['detail'], $_POST['prix'], $_POST['image'], $_POST['categorie'], $_POST['hdIdProduit']);
+	}
+	
+	if(isset($_GET['suppProduit']) && !empty($_GET['suppProduit'])){
+		suppTableauProduit($_GET['suppProduit'], $base, $hote, $utilisateur, $mdp);
+	}
+	
+	creaTableauProduit("produit_id", $base, $hote, $utilisateur, $mdp);
+	
+	if ( isset($_GET['editProduit']) && !empty($_GET['editProduit']) ) {
+		editTableauUser($_GET['editProduit'], $base, $hote, $utilisateur, $mdp); //Affiche le formulaire d'édition d'un produit
+	}else{
+	
+	//Affiche le formulaire d'ajout de produit
+	echo '<h2>Ajouter un produit</h2>';
+	echo '
+		<form class="gestion" method="post" action="gestionProduits.php">
+			<fieldset>
+				<label>Nom :</label><input class="formGestion" type="text" id="nom" name = "nom" /><br/><br/>
+				<label>Description :</label><textarea id="description"  name="description"></textarea><br/><br/>
+				<label>Détail :</label><textarea id="detail" name="detail"></textarea><br/><br/>
+				<label>Prix :</label><input class="formGestion" type="text" id="prix" name = "prix" />  €<br/><br/>
+				<label>Nom du fichier image :</label><input class="formGestion" type="text" id="image" name = "image" /><br/><br/>
+				<label>Catégorie :</label>
+				';
+				listCategorie($base, $hote, $utilisateur, $mdp, null);
+				echo'
+				<input name="effacerAjout" type="reset" value="Effacer" />
+				<input name="envoyerAjout" type="submit" value="Envoyer" />
+			</fieldset>
+		</form>
+	';
+	
+	}
+	}
 }
-
-if(isset($_POST['envoyerModif']) && !empty($_POST['envoyerModif']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['description']) && !empty($_POST['description']) &&isset($_POST['detail']) && !empty($_POST['detail']) &&isset($_POST['prix']) && !empty($_POST['prix']) && isset($_POST['image']) && !empty($_POST['image']) &&isset($_POST['categorie']) && !empty($_POST['categorie']) && isset($_POST['hdIdProduit']) && !empty($_POST['hdIdProduit'])){
-	updateTableauProduit($base, $hote, $utilisateur, $mdp, $_POST['nom'], $_POST['description'], $_POST['detail'], $_POST['prix'], $_POST['image'], $_POST['categorie'], $_POST['hdIdProduit']);
-}
-
-if(isset($_GET['suppProduit']) && !empty($_GET['suppProduit'])){
-	suppTableauProduit($_GET['suppProduit'], $base, $hote, $utilisateur, $mdp);
-}
-
-creaTableauProduit("produit_id", $base, $hote, $utilisateur, $mdp);
-
-if ( isset($_GET['editProduit']) && !empty($_GET['editProduit']) ) {
-	editTableauUser($_GET['editProduit'], $base, $hote, $utilisateur, $mdp); //Affiche le formulaire d'édition d'un produit
-}else{
-
-//Affiche le formulaire d'ajout de produit
-echo '<h2>Ajouter un produit</h2>';
-echo '
-	<form class="gestion" method="post" action="gestionProduits.php">
-		<fieldset>
-			<label>Nom :</label><input class="formGestion" type="text" id="nom" name = "nom" /><br/><br/>
-			<label>Description :</label><textarea id="description"  name="description"></textarea><br/><br/>
-			<label>Détail :</label><textarea id="detail" name="detail"></textarea><br/><br/>
-			<label>Prix :</label><input class="formGestion" type="text" id="prix" name = "prix" />  €<br/><br/>
-			<label>Nom du fichier image :</label><input class="formGestion" type="text" id="image" name = "image" /><br/><br/>
-			<label>Catégorie :</label>
-			';
-			listCategorie($base, $hote, $utilisateur, $mdp, null);
-			echo'
-			<input name="effacerAjout" type="reset" value="Effacer" />
-			<input name="envoyerAjout" type="submit" value="Envoyer" />
-		</fieldset>
-	</form>
-';
-
-}
-}
+	
 echo'
 	</div>
 ';
